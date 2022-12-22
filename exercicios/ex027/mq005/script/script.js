582871c79e8c2a5c1869b3a50be444acf4993836
Raniel Araujo <rani.arsou@gmail.com>
2022-12-22 00:11:47 -0300

    estudando media query

---
+++ exercicios/ex027/mq005/script/script.js
@@ -0,0 +1,17 @@
+const btnBurguer = document.getElementById('burguer');
+const menu = document.getElementById('menu');
+
+
+btnBurguer.addEventListener('click', e => {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none';
+        btnBurguer.style.backgroundColor = 'rgb(233, 210, 0)';
+        btnBurguer.style.color = 'rgb(78, 28, 0)';
+    } else {
+        menu.style.display = 'block';
+        btnBurguer.style.backgroundColor = 'rgb(78, 28, 0)';
+        btnBurguer.style.color = 'rgb(233, 210, 0)';
+    }
+
+
+})
