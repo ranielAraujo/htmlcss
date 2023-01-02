363397bf08cdeba7bef870796241a07845aa456b
Raniel Araujo <rani.arsou@gmail.com>
2023-01-02 18:24:44 -0300

    Desafio login

--- exercicios/ex027/mq005/script/script.js
+++ exercicios/ex027/mq005/script/script.js
@@ -15,3 +15,11 @@ btnBurguer.addEventListener('click', e => {
 
 
 })
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
